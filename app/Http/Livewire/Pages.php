bc779e0d579b1430a05094703616e179986db2bb
Giovane Verbinnen de Oliveira <gioverbinnen@gmail.com>
2021-09-13 18:57:39 -0300

    making Page Component

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,12 +2,124 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Livewire\WithPagination;
+use Illuminate\Validation\Rule;
 
 class Pages extends Component
 {
+    use WithPagination;
+    public $showModalForm = false;
+    public $modelId;
+    public $slug;
+    public $title;
+    public $content;
+
+    public function rules()
+    {
+        return [
+           'title' => 'required',
+           'slug' => ['required', Rule::unique('pages', 'slug')],
+           'content' => 'required',
+        ];
+    }
+
+    public function updatedTitle($value)
+    {
+        $this->slug = Str::slug($this->title);
+    }
+
+    public function create()
+    {
+        $this->validate();
+        Page::create($this->modalData());
+        $this->showModalForm = false;
+        $this->resetVars();
+    }
+
+    public function read()
+    {
+        return Page::paginate(1);
+    }
+
+    public function update()
+    {
+        $this->validate();
+        Page::find($this->modelId)->update($this->modalData());
+        $this->showModalForm = false;
+    }
+
+    public function delete($id)
+    {
+        $page = Page::find($id);
+        $page->delete();
+        $this->showModalForm = false;
+        $this->resetVars();
+    }
+
+    public function modalData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => Str::slug($this->slug),
+            'content' => $this->content,
+        ];
+    }
+
+
+    /**
+     * Show the form modal
+     *
+     * @return void
+     */
+    public function createShowModal()
+    {
+        $this->resetValidation();
+        $this->resetVars();
+        $this->showModalForm = true;
+    }
+
+    public function updateShowModal($id)
+    {
+        $this->resetValidation();
+        $this->resetVars();
+        $this->modelId = $id;
+        $this->showModalForm = true;
+        $this->loadModel();
+    }
+
+    public function loadModel()
+    {
+        $data = Page::find($this->modelId);
+        $this->title = $data->title;
+        $this->slug = $data->slug;
+        $this->content = $data->content;
+    }
+
+    public function resetVars()
+    {
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+        $this->modelId = null;
+    }
+
+    // https://youtu.be/G-ngqfbP5Yk?list=PLSP81gW0XjNHk2D2NREM8A80xWO19Yulj&t=843
+    public function resetPage(Type $var = null)
+    {
+        # code...
+    }
+
+    public function mount()
+    {
+        $this->resetPage();
+    }
     public function render()
     {
-        return view('livewire.pages');
+        return view('livewire.pages', [
+            'data' => $this->read(),
+        ]);
     }
 }
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,3 +1,144 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="p-6">
+    <div class="pb-6">
+        <x-jet-button wire:click="createShowModal">
+            {{__('Create')}}
+        </x-jet-button>
+
+    </div>
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Title
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Content
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    URL
+                                </th>
+                                <th scope="col" colspan="2"
+                                    class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Edit
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @if ($data->count())
+                            @forelse($data as $item)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            {{ $item->title }}
+                                        </div>
+
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">{!! $item->content !!}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <span
+                                        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                        {{$item->slug}}
+                                    </span>
+                                </td>
+                                <td>
+                                    <x-jet-button wire:click="updateShowModal({{ $item->id }})">
+                                        {{__('Update')}}
+                                    </x-jet-button>
+                                    <x-jet-danger-button wire:click="delete({{ $item->id }})">
+                                        {{__('Delete')}}
+                                        </x-jet-button>
+                                </td>
+
+                            </tr>
+                            @empty
+                            <tr>No results</tr>
+                            @endforelse
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-6">
+        {{ $data->links() }}
+    </div>
+
+    <x-jet-dialog-modal wire:model="showModalForm">
+        <x-slot name="title">
+            {{ __('Save Page') }} {{ $modelId }}
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ __('Title') }}" />
+                <x-jet-input id="title" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="title" />
+                @error('title') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ __('Slug') }}" />
+                <div class="mt-1 flex rounded-md shadow-sm">
+                    <span
+                        class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                        http://localhost:8000/
+                    </span>
+                    <input wire:model="slug"
+                        class="form-input flex-1 block w-full rounded-none rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                        placeholder="url-slug">
+                </div>
+                @error('slug') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="mt-4">
+                <label>
+                    <input class="form-checkbox" type="checkbox" value="" wire:model="isSetToDefaultHomePage" />
+                    <span class="ml-2 text-sm text-gray-600">Set as the default home page</span>
+                </label>
+            </div>
+            <div class="mt-4">
+                <label>
+                    <input class="form-checkbox" type="checkbox" value="" />
+                    <span class="ml-2 text-sm text-red-600">Set as the default 404 error page</span>
+                </label>
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ __('Content') }}" />
+                <div class="rounded-md shadow-sm">
+                    <div class="mt-1 bg-white">
+                        <div class="body-content" wire:ignore>
+                            <trix-editor class="trix-content" x-ref="trix" wire:model.debounce.100000ms="content"
+                                wire:key="trix-content-unique-key"></trix-editor>
+                        </div>
+                    </div>
+                </div>
+                @error('content') <span class="error">{{ $message }}</span> @enderror
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('showModalForm')" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+            @if($modelId)
+            <x-jet-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
+                {{ __('Update') }}
+            </x-jet-button>
+            @else
+            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                {{ __('Save') }}
+            </x-jet-button>
+            @endif
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
